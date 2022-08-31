3308b7d50012073ed0698df8f24021bb451afc68
+ <+>
2022-08-31 05:17:37 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+//remove main
+const element = document.getElementById("main");
+element.remove();
+
+//addheader
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent= "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
 // Write your code here!
